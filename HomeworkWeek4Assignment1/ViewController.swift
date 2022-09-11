490967123e81f4281bb9a31c471aa8d8f36155f9
Eddie Duncan <carl.e.duncan@icloud.com>
2022-09-11 11:51:39 -0500

    Initial Commit

---
+++ HomeworkWeek4Assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeworkWeek4Assignment1
+//
+//  Created by Carl Duncan on 9/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
